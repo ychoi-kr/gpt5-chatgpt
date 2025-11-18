9c928c780a45647adf8215308a29834914476036
Yong Choi <ychoi_kr@naver.com>
2025-11-18 17:44:24 +0900

    4장 예제 번역

--- 4_openai_api/5_text_to_speech/streaming_audio.py
+++ 4_openai_api/5_text_to_speech/streaming_audio.py
@@ -2,20 +2,20 @@ import asyncio
 from openai import AsyncOpenAI
 from openai.helpers import LocalAudioPlayer
 
-# クライアントの準備
+# 클라이언트 준비
 openai = AsyncOpenAI()
 
-# メインの定義
+# 메인 함수 정의
 async def main():
     async with openai.audio.speech.with_streaming_response.create(
         model="gpt-4o-mini-tts",
         voice="alloy",
-        input="ワガハイは猫である。名前はまだない。",
-        instructions="明るく前向きな口調で話しましょう。",
+        input="나는 고양이로소이다. 이름은 아직 없다.",
+        instructions="밝고 긍정적인 어조로 말하세요.",
         response_format="pcm",
     ) as response:
         await LocalAudioPlayer().play(response)
 
-# メインの実行
+# 메인 함수 실행
 if __name__ == "__main__":
     asyncio.run(main())
